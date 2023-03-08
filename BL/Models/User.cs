4b2125d7e5a616fa6c4ac4cee6513382b4ad5f6a
Vadim Vologin <106904639+Vadim156@users.noreply.github.com>
2023-03-08 13:24:06 +0200

    Add files via upload

---
+++ BL/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace BusinessLogic.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
